f803ca016960819dbab79eeae97bfee12b025848
adhishakya <shakyaadhish@gmail.com>
2025-03-01 18:50:47 +0545

    table creation query

---
+++ create_table.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS `weather`;
+USE `weather`;
+CREATE TABLE weather_data(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255),
+    temperature float,
+    weather_type varchar(255),
+    weather_description varchar(255),
+    pressure float,
+    humidity float,
+    response_timestamp timestamp,
+    stored_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
