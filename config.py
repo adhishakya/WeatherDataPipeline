f31c094bb61cdcaef3b6ba9203805df2a583edc7
adhishakya <shakyaadhish@gmail.com>
2025-03-05 01:57:37 +0545

    units changed to metric system

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-url = 'https://api.openweathermap.org/data/2.5/weather?q=Kathmandu&appid='+os.getenv('API_KEY')
+url = 'https://api.openweathermap.org/data/2.5/weather?q=Kathmandu&appid='+os.getenv('API_KEY')+'&units=metric'
